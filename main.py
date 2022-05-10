a5273749b32ff01dbdb246a98ea7fac386981a96
ad03 <ad03@ad03.corp.js.com>
2022-05-10 15:03:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+'''
+git clone https://github.com/seanharr11/etlalchemy
+
+mysql damago@10.9.2.20:3306
+passwort: damago
+
+mssql sa@10.9.2.20:1433
+passwort: thu4At7ale7iah5A1
+'''
+
+from etlalchemy import ETLAlchemySource, ETLAlchemyTarget
+import etlalchemy
+
+
+hostname = "10.9.2.20"
+db_name = "Blumenversand_Test"
+
+source = ETLAlchemySource("mssql+pyodbc://sa:thu4At7ale7iah5A1@"+hostname)
+target = ETLAlchemyTarget("mysql://damago:damago@"+hostname+"hostname/db_name", drop_database=True)
+target.addSource(source)
+target.migrate()
